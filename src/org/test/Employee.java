56d7d00bc7c295cc4e9890958e4d46f75d8492f5
NitishGreens <nitishgreens@gmail.com>
2022-07-09 13:33:20 +0530

    Nitish Task Done

---
+++ src/org/test/Employee.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Employee {
+
+	public void test1() {
+		System.out.println("test1");
+	}
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.test1();
+	}
+
+}
